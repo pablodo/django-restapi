f5cccb882e87301c2b018bb6c79bf8b226d1412c
Pablo Díaz Ogni <pablo.diazogni@gmail.com>
2018-05-15 21:18:16 -0300

    Aliases are not required

--- djangorestapi/movies/models.py
+++ djangorestapi/movies/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 
 class Person(models.Model):
-    last_name = models.CharField(max_length=100)
     first_name = models.CharField(max_length=100)
-    aliases = models.CharField(max_length=200)  # JSON with list of aliases
+    last_name = models.CharField(max_length=100)
+    aliases = models.CharField(max_length=200, blank=True)  # JSON with list of aliases
 
 
 class Movie(models.Model):
