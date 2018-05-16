d7e99ee8a07a095fc6c225a3bb2bc904d53b4ed1
Pablo Díaz Ogni <pablo.diazogni@gmail.com>
2018-05-15 21:18:58 -0300

    Created viewsets and mapped urls

--- djangorestapi/movies/views.py
+++ djangorestapi/movies/views.py
@@ -1,5 +1,13 @@
-from django.http import HttpResponse
+from rest_framework import  viewsets
+
+from . import serializers, models
 
 
-def movies(request):
-    return HttpResponse("Movies list not implemented yet", status=501)
+class MovieViewSet(viewsets.ModelViewSet):
+    queryset = models.Movie.objects.all()
+    serializer_class = serializers.MovieSerializer
+
+
+class PersonViewSet(viewsets.ModelViewSet):
+    queryset = models.Person.objects.all()
+    serializer_class = serializers.PersonSerializer
