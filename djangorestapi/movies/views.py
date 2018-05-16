26f641900ac504c67688456cfe0a6da1703d6b75
Pablo Díaz Ogni <pablo.diazogni@gmail.com>
2018-05-16 14:59:55 -0300

    Authenticated unsafe methods

--- djangorestapi/movies/views.py
+++ djangorestapi/movies/views.py
@@ -1,13 +1,32 @@
-from rest_framework import  viewsets
+from rest_framework import viewsets
+from rest_framework import authentication, permissions
 
 from . import serializers, models
 
 
-class MovieViewSet(viewsets.ModelViewSet):
+class AuthMixin(object):
+
+    UNSAFE_ACTIONS = ('destroy', 'create', 'update', 'partial_update')
+
+    authentication_classes = (
+        authentication.SessionAuthentication,
+        authentication.BasicAuthentication,
+    )
+
+    def get_permissions(self):
+        permissions_classes = []
+        if self.action in self.UNSAFE_ACTIONS:
+            permissions_classes.append(permissions.IsAuthenticated)
+        return [p() for p in permissions_classes]
+
+
+class MovieViewSet(AuthMixin, viewsets.ModelViewSet):
     queryset = models.Movie.objects.all()
     serializer_class = serializers.MovieSerializer
 
 
-class PersonViewSet(viewsets.ModelViewSet):
+class PersonViewSet(AuthMixin, viewsets.ModelViewSet):
     queryset = models.Person.objects.all()
     serializer_class = serializers.PersonSerializer
+
+
--- djangorestapi/settings.py
+++ djangorestapi/settings.py
@@ -122,3 +122,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Django Rest Framework config
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+    ),
+}
